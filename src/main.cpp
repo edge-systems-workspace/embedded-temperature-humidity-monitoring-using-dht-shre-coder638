2cd4ab229f24d7553e301e0fe35923c4ea74492e
shre-coder638 <shankarshreyash79@gmail.com>
2026-02-17 20:08:37 +0530

    Serial initialized at 9600 baud rate

--- src/main.cpp
+++ src/main.cpp
@@ -21,14 +21,12 @@ DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
 
-    // TODO 4:
-    // Initialize Serial communication (9600 baud rate)
+    Serial.begin(9600);
 
-    // TODO 5:
-    // Initialize the DHT sensor
+    dht.begin();
 
-    // TODO 6:
-    // Print a system initialization message
+    Serial.println("DHT11 Temperature and Humidity Monitor");
+    Serial.println("System Initialized...");
 }
 
 void loop() {
